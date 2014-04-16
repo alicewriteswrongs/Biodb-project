10fbb0749dd07c9ca969b125b745b5f1e697d9fc
benpote <ben.pote@gmail.com>
2014-04-16 14:29:24 -0400

    Updated! Organized the file a little better

--- fastaintodb.py
+++ fastaintodb.py
@@ -1,12 +1,23 @@
+#!/usr/bin/python
+
 #a quick script to parse a FASTA file and then insert it into a mysqlDB
 #starting with the CSBs, because they seem like the easiest thing to start with
 #this script assumes that the data is located in biodb_project/data (where it is on my laptop)
 #######
 
+#########HEADER
+
 import MySQLdb
+
+#########FUNCTIONS
+
+####DATABASE CONNECTION
 
 #the below is just copied from yozen code
 def connect_db(dbase):
+    """
+    connected to the database dbase, user and password is hardcoded (for now)
+    """
     #make a connection object
     connection = MySQLdb.connect (host="localhost", db=dbase, user="benpote", passwd="password") #highly secure password
     #get a cursor object from that
@@ -14,15 +25,60 @@ def connect_db(dbase):
     return cursor, connection
 
 def close_db(cursor, connection):
+    """
+    close db connection
+    """
     cursor.close()
     connection.close()
 
 def run_query(cursor, query):
+    """
+    Pass it a cursor object and a text mysql query and get results!
+    """
     cursor.execute(query)
     rows = cursor.fetchall()
     return rows
 
-#we want to actually connect to our database
+####FASTA PARSING
+
+
+
+def read_file(file):
+    """
+    Just reads in a file at the location it is passed
+    """
+    with open (file, "r") as myfile:
+        data = myfile.read()
+    return data
+
+##functions for dealing with our various datafiles
+
+def format_csb(data):
+    """
+    Inserts our CSB files into the CSB table, expects a long string
+    (normal result of calling read_file on csb.fasta)
+    Returns a nicely formatted list which we can iterate through with a query function
+    """
+    data = data.split('\n')
+    query = """
+    INSERT 
+    """
+
+
+
+
+
+
+
+########ACTUALLY DOING STUFF!
+
+file = "/home/benpote/Code/biological_databases/group_project/data/csb.fasta"
+
+data = read_file(file)
+
+
+
+#connect to our database
 cursor, connection = connect_db('msad')
 
 #a really boring example query
