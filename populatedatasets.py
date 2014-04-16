9f70f6973d62e2b6045cf13c219eebf831379ccc
benpote <ben.pote@gmail.com>
2014-04-16 17:03:02 -0400

    Correctly populates the dataset table with 2 datasets

--- populatedatasets.py
+++ populatedatasets.py
@@ -3,3 +3,21 @@
 ###This is a quick script to insert data into the dataset table
 ###necessary to have data in this table to insert minicircle data
 ###(datasetid foreign key is not_null in minicircle table)
+
+###add datasets
+
+datasets = {1:'genbank dataset',2:'pacbio dataset'}
+
+def add_datasets(cursor,connection):
+    for i in datasets:
+        query = """
+        INSERT INTO dataset(did,description) VALUES ('%d', '%s');
+        """ % (i,datasets[i])
+        cursor.execute(query)
+    connection.commit()
+
+###run it!
+
+cursor, connection = connect_db('msad')
+add_datasets(cursor,connection)
+close_db(cursor, connection)
