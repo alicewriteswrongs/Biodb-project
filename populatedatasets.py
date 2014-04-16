ff13a030eea857dfbffa5f0950bf384b31bd3faf
benpote <ben.pote@gmail.com>
2014-04-16 16:50:07 -0400

    Script to populate dataset table

---
+++ populatedatasets.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+###This is a quick script to insert data into the dataset table
+###necessary to have data in this table to insert minicircle data
+###(datasetid foreign key is not_null in minicircle table)
