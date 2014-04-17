23660e8ede84bc1ac63040637605d22d8b76435c
benpote <ben.pote@gmail.com>
2014-04-17 12:08:00 -0400

    Drops in the right order

---
+++ droptables.sql
@@ -0,0 +1,8 @@
+-- drop table statements which delete the tables in the right order
+
+DROP TABLE smrna_maps;
+DROP TABLE csb_maps;
+DROP TABLE minicircles;
+DROP TABLE dataset;
+DROP TABLE smallrna;
+DROP TABLE csb;
