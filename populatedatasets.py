84653100ccbc0483fbd531f294ce76b9c86970dc
benpote <ben.pote@gmail.com>
2014-04-21 13:46:31 -0400

    Include database connection functions

--- populatedatasets.py
+++ populatedatasets.py
@@ -6,6 +6,36 @@
 
 ###add datasets
 
+import MySQLdb
+
+##Database connection stuff
+def connect_db(dbase):
+    """
+    connected to the database dbase, user and password is hardcoded (for now)
+    """
+    #make a connection object
+    connection = MySQLdb.connect (host="localhost", db=dbase, user="benpote", passwd="password") #highly secure password
+    #get a cursor object from that
+    cursor = connection.cursor()
+    return cursor, connection
+
+def close_db(cursor, connection):
+    """
+    close db connection
+    """
+    cursor.close()
+    connection.close()
+
+def run_query(cursor, query):
+    """
+    Pass it a cursor object and a text mysql query and get results!
+    """
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    return rows
+
+
+
 #currently just adds two datasets, pretty easy to add more
 #(delete records in the table, and more to the dictionary below, rerun script)
 datasets = {1:'genbank dataset',2:'pacbio dataset',3:'hong-simpson dataset'}
